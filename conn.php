1b2d3b7cf18fe73f7a48ea517b3aaeda34197423
Zoe Liu <snow.baby.msa.hinet.net2122@Zoes-MacBook-Air.local>
2022-03-22 14:30:27 +0800

    conn update

--- conn.php
+++ conn.php
@@ -1,6 +1,11 @@
 <?php
+    $username = 'be284ce1d282ac';
+    $password = '637a76b6';
+    $hostname = 'us-cdbr-east-05.cleardb.net';
+    $database = 'heroku_e6bb006cadde687';
+
     try {
-        $conn = new PDO("mysql:host=localhost;dbname=photoSharingApp", 'root', 'babu040621');
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
     } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
