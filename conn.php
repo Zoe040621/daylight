e074e27c461cff7ac6b08c7f42c9d35bff76ae40
Zoe Liu <snow.baby.msa.hinet.net2122@Zoes-MacBook-Air.local>
2022-03-22 14:32:21 +0800

    update conn

--- conn.php
+++ conn.php
@@ -1,11 +1,11 @@
 <?php
     $username = 'be284ce1d282ac';
-    $password = '637a76b6';
-    $hostname = 'us-cdbr-east-05.cleardb.net';
-    $database = 'heroku_e6bb006cadde687';
+    $pw = '637a76b6';
+    $host = 'us-cdbr-east-05.cleardb.net';
+    $db = 'heroku_e6bb006cadde687';
 
     try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn = new PDO("mysql:host=$host;dbname=$db", $username, $pw);
     } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
